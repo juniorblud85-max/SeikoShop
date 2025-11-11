4e51c21e1bcd492c39e0bed31393cc50936cb1ee
juniorblud85-max <juniorblud85@gmail.com>
2025-11-11 15:06:16 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: "sans-serif", textAlign: "center", marginTop: "3rem" }}>
+      <h1>🕰️ SeikoShop</h1>
+      <p>Bienvenue sur la marketplace Seiko !</p>
+      <p>Ce site est prêt à être déployé sur Vercel.</p>
+    </div>
+  )
+}
